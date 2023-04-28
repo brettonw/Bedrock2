41cc08a347131944665e6d4c967d5bb0a4630292
Bretton Wade <bretton.wade@gmail.com>
2023-04-28 18:31:28 -0400

    development - update to current libs, add error handler to https

--- applications/bedrock-site/src/main/dist/http.js
+++ applications/bedrock-site/src/main/dist/http.js
@@ -1,7 +1,7 @@
 Bedrock.Http = function () {
     let $ = Object.create (null);
 
-    $.get = function (queryString, onSuccess) {
+    $.get = function (queryString, onSuccess, onError = (event) => { LOG(event); }) {
         let request = new XMLHttpRequest ();
         request.onload = function (event) {
             if (request.status === 200) {
@@ -9,11 +9,12 @@ Bedrock.Http = function () {
                 onSuccess (response);
             }
         };
+        request.onerror = onError;
         request.open ("GET", queryString, true);
         request.send ();
     };
 
-    $.post = function (queryString, postData, onSuccess) {
+    $.post = function (queryString, postData, onSuccess, onError = (event) => { LOG(event); }) {
         let request = new XMLHttpRequest ();
         request.onload = function (event) {
             if (request.status === 200) {
@@ -21,6 +22,7 @@ Bedrock.Http = function () {
                 onSuccess (response);
             }
         };
+        request.onerror = onError;
         request.open ("POST", queryString, true);
         request.send (postData);
     };
